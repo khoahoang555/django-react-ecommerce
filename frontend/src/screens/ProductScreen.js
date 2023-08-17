9dfa9ec6de6982e8a6156b59b97a50a4e2e03cb6
khoa pham <khoa.pham@tpssoft.com>
2023-08-17 18:20:01 +0700

    Front-end: Create project

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
+
+function ProductScreen({ match }) {
+    const productId = useParams().id;
+    const product = products.find((p) => p._id == productId)
+    return (
+        <div>
+            {product.name}
+        </div>
+    )
+}
+
+export default ProductScreen;
